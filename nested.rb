8bb337420d0917f587742275422c3243dfd67b7d
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 12:58:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,7 @@ def adding_matz
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
+          :known_for == "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
